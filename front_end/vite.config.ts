bc8a94ae7f4c1329ce7edea250b8c9e47375fa39
Todd Yoshioka <toddhy@hawaii.edu>
2026-04-18 12:32:18 -1000

    Clean up README. Fix python format comment in vite config file.

--- front_end/vite.config.ts
+++ front_end/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', # Listen on all network interfaces
+    host: '0.0.0.0', // Listen on all network interfaces
   },
 })
